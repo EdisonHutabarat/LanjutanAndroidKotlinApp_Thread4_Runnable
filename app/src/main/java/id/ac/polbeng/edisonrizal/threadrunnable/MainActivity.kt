2a7ef635b237ab88c3b3fb600ad8edafe549b43c
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-10-30 11:55:50 +0700

    add MainActivity.kt and last commmit

--- app/src/main/java/id/ac/polbeng/edisonrizal/threadrunnable/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/edisonrizal/threadrunnable/MainActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.edisonrizal.threadrunnable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate( layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val runnable = Worker()
+            val thread = Thread(runnable)
+            thread.start()
+        }
+    }
+
+    inner class Worker : Runnable {
+        override fun run() {
+            killSomeTime()
+        }
+    }
+
+    private fun killSomeTime() {
+        for (i in 1..20) {
+            Thread.sleep(2000)
+            println("i: $i")
         }
     }
 }

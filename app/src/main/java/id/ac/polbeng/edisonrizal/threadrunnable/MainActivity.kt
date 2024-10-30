f303ac13cbd6a81dd8d317a18938aa5a1babf1cf
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-10-30 22:18:21 +0700

    add menggunakan class handler dan AsyncTask

--- app/src/main/java/id/ac/polbeng/edisonrizal/threadrunnable/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/edisonrizal/threadrunnable/MainActivity.kt
@@ -1,35 +1,35 @@
 package id.ac.polbeng.edisonrizal.threadrunnable
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import id.ac.polbeng.edisonrizal.threadrunnable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate( layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.button.setOnClickListener {
-            Thread {
-                killSomeTime()
-            } .start()
+            Worker().execute()
         }
     }
-
-    private fun killSomeTime() {
-        for (i in 1..20) {
-            runOnUiThread{
-                binding.textView.text = i.toString()
+    @SuppressLint("StaticFieldLeak")
+    inner class Worker : AsyncTask<Void, String, Boolean>() {
+        override fun doInBackground(vararg p0: Void?): Boolean {
+            for (i in 1..20) {
+                publishProgress(i.toString())
+                Thread.sleep(2000)
             }
-            Thread.sleep(2000)
-            println("i: $i")
+            return true
+        }
+        override fun onProgressUpdate(vararg values: String?) {
+            binding.textView.text = values[0]
+        }
+        override fun onPostExecute(result: Boolean?) {
+            println(result)
         }
     }
 }
